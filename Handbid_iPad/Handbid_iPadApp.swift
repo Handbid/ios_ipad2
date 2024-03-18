3c154fd8408052c7c71d895e86c1cf8e76dd4143
BakJakub <kuba.bak.1997@gmail.com>
2024-03-18 08:37:37 +0100

    Created project and added hierarchy folders

---
+++ Handbid_iPad/Handbid_iPadApp.swift
@@ -0,0 +1,17 @@
+//
+//  Handbid_iPadApp.swift
+//  Handbid_iPad
+//
+//  Created by Jakub Bak on 18/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct Handbid_iPadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Handbid_iPadUITests/Handbid_iPadUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Handbid_iPadUITestsLaunchTests.swift
+//  Handbid_iPadUITests
+//
+//  Created by Jakub Bak on 18/03/2024.
+//
+
+import XCTest
+
+final class Handbid_iPadUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
