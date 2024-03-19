a05037db19d205d8f76be6045863695ec0bca844
BakJakub <kuba.bak.1997@gmail.com>
2024-03-19 10:13:43 +0100

    Added CoordinatorBase

--- Handbid_iPad/Coordinator/RegistrationCoordinator.swift
+++ Handbid_iPad/Coordinator/RegistrationCoordinator.swift
@@ -2,20 +2,6 @@
 
 import SwiftUI
 
-struct RootPageView<T: PageProtocol>: View {
-	@EnvironmentObject private var coordinator: RegistrationCoordinator<T>
-
-	var body: some View {
-		NavigationStack(path: $coordinator.navigationStack) {
-			coordinator.build(page: RegistrationPage.getStarted as! T)
-				.navigationDestination(for: T.self) { page in
-					coordinator.build(page: page)
-				}
-		}
-		.environmentObject(coordinator)
-	}
-}
-
 protocol PageProtocol: RawRepresentable, Identifiable, Hashable where RawValue == String {}
 
 enum RegistrationPage: String, PageProtocol, Identifiable, Hashable {
@@ -25,67 +11,122 @@ enum RegistrationPage: String, PageProtocol, Identifiable, Hashable {
 	}
 }
 
-@MainActor
-class RegistrationCoordinator<T: PageProtocol>: ObservableObject {
-	@Published var navigationStack = [T]()
-	@Published var pages = [T]()
-	@Published var currentPageIndex: Int = 0
-	var viewModels: [T: Any] = [:]
-	var userRegistration: UserRegistrationModel?
+protocol CoordinatorBase {
+	associatedtype PageType: PageProtocol
+	associatedtype PageView: View
+	var navigationStack: [PageType] { get set }
+	var pages: [PageType] { get set }
+	var currentPageIndex: Int { get set }
+	var viewModels: [PageType: [Any]] { get set }
 
-	var isLoggedIn: Bool = false {
-		didSet {
-			if isLoggedIn {
-				popToRoot()
-			}
-		}
-	}
+	mutating func push(_ page: PageType, with models: [Any]?)
+	mutating func pop()
+	mutating func popToRoot()
+	func build(page: PageType) -> PageView
+	func viewForCurrentPage() -> PageView
+	mutating func navigateTo(_ index: Int)
+}
 
-	func push(_ page: T, with userRegistration: UserRegistrationModel? = nil) {
+extension CoordinatorBase where PageType == RegistrationPage, PageView: View {
+	mutating func push(_ page: PageType, with models: [Any]?) {
 		navigationStack.append(page)
-		viewModels[page] = userRegistration
-
-		if let userRegistration {
-			self.userRegistration = userRegistration
+		if let models {
+			viewModels[page] = models
 		}
 	}
 
-	func pop() {
+	mutating func pop() {
 		guard !navigationStack.isEmpty else { return }
 		navigationStack.removeLast()
 	}
 
-	func popToRoot() {
+	mutating func popToRoot() {
 		navigationStack.removeAll()
 	}
 
-	@ViewBuilder
-	func build(page: T) -> some View {
+	func build(page: PageType) -> PageView {
 		switch page {
-		case let registrationPage as RegistrationPage:
-			switch registrationPage {
-			case .getStarted:
-				GetStartedView<RegistrationPage>()
-			case .logIn:
-				LogInView<RegistrationPage>()
-			}
-
-		default:
-			EmptyView()
+		case .getStarted:
+			return AnyView(GetStartedView<RegistrationPage>()) as! PageView
+		case .logIn:
+			return AnyView(LogInView<RegistrationPage>()) as! PageView
 		}
 	}
 
-	@ViewBuilder
-	func viewForCurrentPage() -> some View {
+	func viewForCurrentPage() -> PageView {
 		if pages.indices.contains(currentPageIndex) {
-			build(page: pages[currentPageIndex])
+			return build(page: pages[currentPageIndex])
 		}
 		else {
-			Text("No page selected")
+			return AnyView(Text("No page selected")) as! PageView
 		}
 	}
 
-	func navigateTo(_ index: Int) {
+	mutating func navigateTo(_ index: Int) {
+		guard pages.indices.contains(index) else { return }
+		currentPageIndex = index
+	}
+}
+
+protocol PageView: View {
+	associatedtype PageType: PageProtocol
+}
+
+struct RegistrationCoordinator<T: PageProtocol, PageViewType: PageView>: CoordinatorBase where T == PageViewType.PageType {
+	typealias PageType = T
+	typealias PageView = PageViewType
+
+	var navigationStack = [T]()
+	var pages = [T]()
+	var viewModels: [T: [Any]] = [:] // Change to accept an array of models
+	var currentPageIndex: Int = 0
+	var userRegistration: UserRegistrationModel?
+	let registrationPageType: RegistrationPage.Type // Additional parameter
+
+	init(registrationPageType: RegistrationPage.Type) { // Additional initializer
+		self.registrationPageType = registrationPageType
+	}
+
+	mutating func push(_ page: T, with models: [Any]?) { // Update to accept an array of models
+		navigationStack.append(page)
+		if let models {
+			viewModels[page] = models
+		}
+	}
+
+	mutating func pop() {
+		guard !navigationStack.isEmpty else { return }
+		navigationStack.removeLast()
+	}
+
+	mutating func popToRoot() {
+		navigationStack.removeAll()
+	}
+
+	func build(page: T) -> PageView {
+		switch page {
+		case let page as RegistrationPage:
+			switch page {
+			case .getStarted:
+				return AnyView(GetStartedView<T>()) as! PageView
+			case .logIn:
+				return AnyView(LogInView<T>()) as! PageView
+			}
+		default:
+			return AnyView(Text("Unknown page")) as! PageView
+		}
+	}
+
+	func viewForCurrentPage() -> PageView {
+		if pages.indices.contains(currentPageIndex) {
+			return build(page: pages[currentPageIndex])
+		}
+		else {
+			return AnyView(Text("No page selected")) as! PageView
+		}
+	}
+
+	mutating func navigateTo(_ index: Int) {
 		guard pages.indices.contains(index) else { return }
 		currentPageIndex = index
 	}
--- Handbid_iPad/Views/Registration/GetStartedView.swift
+++ Handbid_iPad/Views/Registration/GetStartedView.swift
@@ -2,11 +2,14 @@
 
 import SwiftUI
 
-struct GetStartedView<T: PageProtocol>: View {
-	@EnvironmentObject private var coordinator: RegistrationCoordinator<T>
+struct GetStartedView<T: PageProtocol>: View, PageView {
+	typealias PageType = T
+
+	let coordinator = RegistrationCoordinator<T, GetStartedView<T>>(registrationPageType: RegistrationPage.self)
 
 	var body: some View {
 		Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+		//        coordinator.push(RegistrationPage.logIn, with: nil)
 	}
 }
 
