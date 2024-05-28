763ac2ed075d14d1f735d6488e4655a3021bf148
BakJakub <kuba.bak.1997@gmail.com>
2024-05-28 11:39:16 +0200

    Added missing import

--- Handbid_iPad/Supporting-Methods/DeviceConfigurator.swift
+++ Handbid_iPad/Supporting-Methods/DeviceConfigurator.swift
@@ -1,7 +1,9 @@
 // Copyright (c) 2024 by Handbid. All rights reserved.
 
+import UIKit
+
 enum DeviceConfigurator {
 	static var isSidebarAlwaysVisible: Bool {
-		UIDevice.current.userInterfaceIdiom == .pad
+                UIDevice.current.userInterfaceIdiom == .pad
 	}
 }
--- Handbid_iPad/Supporting-Methods/DeviceContext.swift
+++ Handbid_iPad/Supporting-Methods/DeviceContext.swift
@@ -1,5 +1,7 @@
 // Copyright (c) 2024 by Handbid. All rights reserved.
 
+import UIKit
+
 class DeviceContext: ObservableObject {
 	@Published var isPad: Bool = UIDevice.current.userInterfaceIdiom == .pad
 	@Published var isPhone: Bool = UIDevice.current.userInterfaceIdiom == .phone
