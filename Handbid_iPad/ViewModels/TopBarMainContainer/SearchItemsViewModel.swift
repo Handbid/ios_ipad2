bce05a521c9b3ea18f62cbbcd0f0d039e2ec7f53
Bąk Jakub <kuba.bak.1997@gmail.com>
2024-06-07 13:32:13 +0200

    Updated design for  SearchItems

--- Handbid_iPad/ViewModels/TopBarMainContainer/SearchItemsViewModel.swift
+++ Handbid_iPad/ViewModels/TopBarMainContainer/SearchItemsViewModel.swift
@@ -3,15 +3,31 @@
 import Combine
 import SwiftUI
 
-class SearchItemsViewModel: ObservableObject, ViewModelTopBarProtocol {
-	var actions: [TopBarAction] { [] }
+class SearchItemsViewModel: ObservableObject {
 	private let dataManager: DataManager?
+	@Published var searchText: String = ""
+	@Published var filteredItems: [ItemModel] = []
+
+	private var items: [ItemModel] = []
 
 	init(dataManager: DataManager? = nil) {
 		self.dataManager = dataManager
+		// Fetch items from dataManager or other source
+		self.items = fetchItems()
+		self.filteredItems = items
 	}
 
-	var centerViewData: TopBarCenterViewData {
-		TopBarCenterViewData(type: .custom, customView: AnyView(EmptyView()))
+	func search() {
+		if searchText.isEmpty {
+			filteredItems = items
+		}
+		else {
+			// filteredItems = items.filter { $0.contains(searchText)! }
+		}
+	}
+
+	private func fetchItems() -> [ItemModel] {
+		// Fetch or generate items
+		[]
 	}
 }
--- Handbid_iPad/Views/TopBarMainContainer/SearchItemsView.swift
+++ Handbid_iPad/Views/TopBarMainContainer/SearchItemsView.swift
@@ -6,12 +6,21 @@ struct SearchItemsView<T: PageProtocol>: View {
 	@EnvironmentObject private var coordinator: Coordinator<T, Any?>
 	@ObservedObject var searchItemsViewModel: SearchItemsViewModel
 	@StateObject var deviceContext = DeviceContext()
+	@FocusState private var focusedField: Field?
 
 	var body: some View {
 		VStack(spacing: 0) {
 			topBarContent(for: .searchItems)
 				.accessibility(identifier: "topBar")
+				.frame(height: 150)
 			GeometryReader { _ in
+				ScrollView {
+					LazyVStack {
+						//                        ForEach(searchItemsViewModel.filteredItems, id: \.self) { item in
+						//                            Text(item.name) // replace with your custom cell view
+						//                        }
+					}
+				}
 			}
 		}
 		.navigationBarBackButtonHidden()
@@ -20,7 +29,33 @@ struct SearchItemsView<T: PageProtocol>: View {
 	private func topBarContent(for viewType: MainContainerTypeSubPagesView) -> some View {
 		switch viewType {
 		case .searchItems:
-			AnyView(GenericTopBarContentFactory(viewModel: searchItemsViewModel, deviceContext: deviceContext).createTopBarContentWithoutLogo())
+			AnyView(
+				VStack(spacing: 5) {
+					HStack {
+						FormField(
+							fieldType: .searchBar,
+							labelKey: LocalizedStringKey("search_item_label"),
+							hintKey: LocalizedStringKey("search_item_label"),
+							fieldValue: $searchItemsViewModel.searchText,
+							focusedField: _focusedField
+						)
+
+						Button(action: {
+							coordinator.pop()
+						}) {
+							Image("closeIcon")
+								.scaledToFit()
+								.foregroundColor(.gray)
+								.frame(width: 60, height: 60)
+						}
+					}
+					.padding([.leading, .trailing], 30)
+
+					Text("\(searchItemsViewModel.filteredItems.count) results for \"\(searchItemsViewModel.searchText)\"")
+						.padding(.top, 8)
+						.foregroundColor(.gray)
+				}
+			)
 		}
 	}
 }
