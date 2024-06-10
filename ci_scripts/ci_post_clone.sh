a41cc845c0f0c164ec18e4876b7fa389168fa47b
BakJakub <kuba.bak.1997@gmail.com>
2024-06-10 23:26:34 +0200

    Added automatically generating test notes

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -4,7 +4,3 @@ defaults write com.apple.dt.Xcode IDESkipPackagePluginFingerprintValidatation -b
 defaults write com.apple.dt.Xcode IDESkipMacroFingerprintValidation -bool YES
 defaults delete com.apple.dt.Xcode IDEPackageOnlyUseVersionsFromResolvedFile
 defaults delete com.apple.dt.Xcode IDEDisableAutomaticPackageResolution
-
-if [[ -d "$CI_APP_STORE_SIGNED_APP_PATH" ]]; then
-    git fetch --deepen 1 && git log -1 --pretty=format:"%s" | cat > ../TestFlight/WhatToTest.en-US.txt
-fi
--- ci_scripts/ci_post_xcodebuild.sh
+++ ci_scripts/ci_post_xcodebuild.sh
@@ -1,1 +1,12 @@
 #!/bin/sh
+
+if [[ $CI_WORKFLOW == "TestFlight" ]]; then
+  pushd ..
+    mkdir TestFlight
+    pushd TestFlight
+      for locale in en-GB en-US; do
+        git fetch --deepen 1 && git log -1 --pretty=format:"%s" > WhatToTest.$locale.txt
+      done
+    popd
+  popd
+fi
