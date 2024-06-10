cfd1ab2e59b72736b77731725893fdb6ba476303
BakJakub <kuba.bak.1997@gmail.com>
2024-06-10 23:39:15 +0200

    Disabled bitcode, changed script to generate description in testflight

--- ci_scripts/ci_post_xcodebuild.sh
+++ ci_scripts/ci_post_xcodebuild.sh
@@ -1,12 +1,5 @@
 #!/bin/sh
 
-if [[ $CI_WORKFLOW == "TestFlight" ]]; then
-  pushd ..
-    mkdir TestFlight
-    pushd TestFlight
-      for locale in en-GB en-US; do
-        git fetch --deepen 1 && git log -1 --pretty=format:"%s" > WhatToTest.$locale.txt
-      done
-    popd
-  popd
+if [[ -d "$CI_APP_STORE_SIGNED_APP_PATH" ]]; then
+    git fetch --deepen 1 && git log -1 --pretty=format:"%s" | cat > ../TestFlight/WhatToTest.en-US.txt
 fi
